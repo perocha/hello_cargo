0dfba2eb5e81482f15b4f29f208543ff053dc913
perocha <pedrojrocha@outlook.com>
2022-09-11 12:22:52 +0200

    Add a module with loops examples

---
+++ src/utils/loops.rs
@@ -0,0 +1,10 @@
+pub fn execute_loops(){
+    loop {
+        // Keep printing, printing, printing...
+        println!("We loop forever!");
+        // On the other hand, maybe we should stop!
+        break;                            
+    }
+
+
+}
--- src/utils/mod.rs
+++ src/utils/mod.rs
@@ -1,3 +1,4 @@
 pub mod module;
 pub mod extra;
 pub mod hashmap;
+pub mod loops;
